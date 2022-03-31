cd085b0157b499d7d83a6bf0d82f41eab7b548c7
Raffael Sahli <raffael.sahli@doodle.com>
2022-03-31 10:33:44 +0200

    fix condition

--- scripts/e2e/test.sh
+++ scripts/e2e/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kubectl -n default apply -f config/testdata/rule.yaml
-kubectl -n default wait prometheuspatchrule/annotate-namespace --for=condition=Ready --timeout=1m
+kubectl -n default wait prometheuspatchrule/annotate-namespace --for=condition=Active --timeout=1m
 kubectl -n default wait prometheuspatchrule/annotate-namespace --for=condition=PatchApplied --timeout=15s
 test $(kubectl get ns default -o=jsonpath='{.metadata.annotations.foo}') == "bar"
