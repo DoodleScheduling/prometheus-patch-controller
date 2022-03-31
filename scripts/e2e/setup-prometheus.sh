4833796dadd5ab89e0a7c3be4ebeb2cdf166b6ff
Raffael Sahli <raffael.sahli@doodle.com>
2022-03-31 09:59:30 +0200

    wait for prom

--- scripts/e2e/setup-prometheus.sh
+++ scripts/e2e/setup-prometheus.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 kubectl create ns prometheus
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
+helm repo update prometheus-community
 helm install prometheus prometheus-community/prometheus \
   --version "15.6.0" \
   --namespace prometheus \
+  --wait \
   --set kubeStateMetrics.enabled=false \
   --set nodeExporter.enabled=false \
   --set alertmanager.enabled=false \
